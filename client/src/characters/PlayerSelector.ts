f2ab0a9254ea7fe006d2765262846dd8dd785126
Prashant Jha <prjha@C02X60DSJG5J.ad.here.com>
2021-11-26 17:15:42 +0530

    loom SDK integrations

--- client/src/characters/PlayerSelector.ts
+++ client/src/characters/PlayerSelector.ts
@@ -3,6 +3,13 @@ import MyPlayer from './MyPlayer'
 import { PlayerBehavior } from '../../../types/PlayerBehavior'
 import Item from '../items/Item'
 
+// loom sdk
+import { isSupported, setup } from "@loomhq/loom-sdk";
+
+// for env
+const API_KEY = "17dee62d-9d46-4523-acc3-b7de86f6938f";
+const BUTTON_ID = "loom-sdk-button";
+
 export default class PlayerSelector extends Phaser.GameObjects.Zone {
   selectedItem?: Item
 
@@ -16,11 +23,45 @@ export default class PlayerSelector extends Phaser.GameObjects.Zone {
     if (!cursors) {
       return
     }
-
+    
     // no need to update player selection while sitting
     if (player.playerBehavior === PlayerBehavior.SITTING) {
       return
     }
+
+    // loomSDK
+  async function init() {
+      const {
+          supported,
+          error
+      } = await isSupported();
+      if (!supported) {
+          console.warn(`Error setting up Loom: ${error}`);
+          return;
+      }
+      const root = document.getElementById("root");
+      if (!root) {
+          return;
+      }
+      root.innerHTML = `<buttton class = "button-grid" style="transform: translateX(20px);"> <button id="${BUTTON_ID}">Record Video Status</button> </div>`;
+      const button = document.getElementById(BUTTON_ID);
+      if (!button) {
+          return;
+      }
+      const {
+          configureButton
+      } = await setup({
+          apiKey: API_KEY
+      });
+      const sdkButton = configureButton({
+          element: button
+      });
+      sdkButton.on("insert-click", async video => {
+          console.log(video)
+          console.log(video.embedUrl)
+      });
+      
+  }
 
     // update player selection box position so that it's always in front of the player
     const { x, y } = player
@@ -32,7 +73,12 @@ export default class PlayerSelector extends Phaser.GameObjects.Zone {
       this.setPosition(x, y - 32)
     } else if (cursors.down?.isDown) {
       this.setPosition(x, y + 32)
-    }
+    } else if (cursors.space?.isDown) {
+      init();
+      console.log("space")
+    } else if (cursors.shift?.isDown) {
+      console.log("shift")
+    } 
 
     // while currently selecting an item,
     // if the selector and selection item stop overlapping, clear the dialog box and selected item
